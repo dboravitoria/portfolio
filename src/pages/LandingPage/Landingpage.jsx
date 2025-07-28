9b870520ffbfbe8f5aeb88040fa04717f6fa5974
Débora <dboravitoriasilva16@gmail.com>
2025-07-27 21:02:58 -0300

    [FIX] Ajustando pasta da landingPage

---
+++ src/pages/LandingPage/Landingpage.jsx
@@ -0,0 +1,15 @@
+import Home from '../Home/Home'
+import About from '../About/About'
+import Projects from '../Projects/Projects'
+import Contact from '../Contact/Contact'
+
+export default function LadingPage(){
+    return(
+        <>
+            <Home/>
+            <About/>
+            <Projects/>
+            <Contact/>
+        </>
+    )
+}
--- src/router.jsx
+++ src/router.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
 import Projects from "./pages/Projects/Projects";
 import Contact from "./pages/Contact/Contact";
-import Landingpage from "./pages/Landingpage";
+import Landingpage from './pages/LandingPage/Landingpage'
 
 export default function AppRoutes() {
   return (

e4828843ad36e82bd3c0f449f0b2465982d8fd94
Débora <dboravitoriasilva16@gmail.com>
2025-08-01 00:07:14 -0300

    [FIX] Ajustando caminhos para a página sobre e projetos

--- src/pages/Home/elements/Button.jsx
+++ src/pages/Home/elements/Button.jsx
@@ -1,19 +1,31 @@
-export default function Button({link, nameButton, target,title, children}){
-    
-    return(
-        <>
-            <a href={link} nameButton={nameButton} target={target} title={title} className="
-            text-secondaryDark dark:text-white
-            dark:bg-white/10 bg-secondaryDark/10
-            backdrop-blur-md  p-2 rounded-full border-2
-            dark:border-white/15 border-secondaryDark/15
-            shadow-custom transition 
-            hover:bg-white dark:hover:bg-white dark:hover:text-secondaryDark
-            hover:drop-shadow-custom dark:hover:border-2 dark:hover:border-secondaryDark/20
-            hover:scale-105 cursor-pointer
-            sm:text-sm md:text-md lg:text-lg">
+import { Link } from "react-router-dom";
+
+export default function Button({ link, target, title, children, download }) {
+    const isInternal = link.startsWith("/");
+
+    const className = `
+        text-secondaryDark dark:text-white
+        dark:bg-white/10 bg-secondaryDark/10
+        backdrop-blur-md p-2 rounded-full border-2
+        dark:border-white/15 border-secondaryDark/15
+        shadow-custom transition 
+        hover:bg-white dark:hover:bg-white dark:hover:text-secondaryDark
+        hover:drop-shadow-custom dark:hover:border-2 dark:hover:border-secondaryDark/20
+        hover:scale-105 cursor-pointer
+        sm:text-sm md:text-md lg:text-lg
+    `;
+
+    if (isInternal) {
+        return (
+            <Link to={link} title={title} className={className}>
+                {children}
+            </Link>
+        );
+    } else {
+        return (
+            <a href={link} target={target} title={title} download={download} className={className}>
                 {children}
             </a>
-        </>
-    );
+        );
+    }
 }

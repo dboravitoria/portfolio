17c867160d209c4e1f1bc1d727add4e59cdc9897
Débora <dboravitoriasilva16@gmail.com>
2025-09-03 13:15:59 -0300

    [FIX] Ajustando página de contato para o formulário enviar e-mail

--- src/pages/Contact/Main.jsx
+++ src/pages/Contact/Main.jsx
@@ -1,50 +1,109 @@
+import { useState } from "react";
 // eslint-disable-next-line no-unused-vars
-import { motion } from 'framer-motion'
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
-import Label from "./elements/Label"
-import Title from "./elements/Title"
-import Input from "./elements/Input"
-import Button from './elements/Button'
-import WrapperBlur from "./elements/WrapperBlur"
+import Label from "./elements/Label";
+import Title from "./elements/Title";
+import Input from "./elements/Input";
+import Button from "./elements/Button";
+import WrapperBlur from "./elements/WrapperBlur";
+
 
 export default function Main() {
+  const [form, setForm] = useState({ nome: "", email: "", mensagem: "" });
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setStatus("Enviando...");
+
+    emailjs.send(
+        "service_8kg1ltj",
+        "template_lvpftve",
+        {
+            user_name: form.nome,
+            user_email: form.email,
+            message: form.mensagem,
+        },
+        "AwQOMby3IGkWMjOyj"
+        )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          setStatus("Mensagem enviada com sucesso!");
+          setForm({ nome: "", email: "", mensagem: "" });
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          setStatus("Erro ao enviar a mensagem.");
+        }
+      );
+  };
 
   return (
     <>
-    <Title title={"Vamos construir algo incrível juntos?"}/>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6 }}>
-        
-        <section className=" mt-52 sm:-mt-20 md:mt-10 lg:mt-10 p-2">
-            <section className='row justify-content-center'>
-                <section className="col-12 col-sm-10 col-md-5 text-center sm:mt-72 md:mt-48 mb-20">
-                <WrapperBlur>
-                    <h3 className='text-2xl text-secondaryDark dark:text-white/90 mb-2 font-primary'>🚨 Página com Formulário de Contato em Desenvolvimento ⛏</h3>
-                    <p className=' text-secondaryDark dark:text-white/90 mb-2 font-secondary font-semibold'>Em breve, essa página terá um formulário de envio de e-mail para contato direto comigo! 😊</p>
-                    <p className=' text-secondaryDark dark:text-white/90 mb-2 font-secondary font-semibold'>Estou evoluindo minhas habilidades de back-end para oferecer mais recursos e uma melhor experiência!</p>
-                    <p className=' text-secondaryDark dark:text-white/90 mb-2 font-secondary font-semibold'>No momento, você pode entrar em contato comigo pelos links disponiblizados logo abaixo no rodapé da página. ⬇️</p>
-                    <p className='text-xl text-secondaryDark dark:text-white/90 mb-2 font-secondary font-semibold'>Desde já agradeço pela sua visualização!</p>
-                    {/* <form className="flex flex-col gap-2 text-left">
-                        <div>
-                            <Label title="Nome" />
-                            <Input title="nome" type="text" placeholder="Digite seu nome..." />
-                        </div>
+      <Title title={"Vamos construir algo incrível juntos?"} />
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.6 }}>
+        <section className="mt-52 sm:-mt-20 md:mt-10 lg:mt-10 p-2">
+          <section className="row justify-content-center">
+            <section className="col-12 col-sm-10 col-md-5 text-center sm:mt-72 md:mt-48 mb-24">
+              <WrapperBlur>
+                
+                <h2 className="text-secondaryDark dark:text-white/90 mb-3 font-secondary font-semibold text-lg">
+                  Preencha o formulário abaixo para me enviar uma mensagem!
+                </h2>
 
-                        <div>
-                            <Label title="E-mail" />
-                            <Input title="email" type="email" placeholder="Digite seu e-mail..." />
-                        </div>
+                <form onSubmit={handleSubmit} className="flex flex-col gap-2 text-left">
+                  <div>
+                    <Label title="Nome" />
+                    <Input
+                      name="nome"
+                      type="text"
+                      value={form.nome}
+                      onChange={handleChange}
+                      placeholder="Digite seu nome..."
+                    />
+                  </div>
 
-                        <div>
-                            <Label title="Mensagem" />
-                            <Input title="mensagem" type="textarea" placeholder="Olá Débora, gostaria de dizer..." />
-                        </div>
-                        <Button title={"Enviar"}/>
-                    </form> */}
-                </WrapperBlur>
-                </section>
+                  <div>
+                    <Label title="E-mail" />
+                    <Input
+                      name="email"
+                      type="email"
+                      value={form.email}
+                      onChange={handleChange}
+                      placeholder="Digite seu e-mail..."
+                    />
+                  </div>
+
+                  <div>
+                    <Label title="Mensagem" />
+                   <textarea
+                        name="mensagem"
+                        type="textarea"
+                        value={form.mensagem}
+                        onChange={handleChange}
+                        placeholder="Digite sua mensagem..."
+                        className="rounded-xl ml-2 backdrop-blur-md 
+                        p-2 h-40 shadow-lg drop-shadow-custom mb-2 text-secondaryDark dark:text-white block mx-auto
+                        dark:shadow-white/10 dark:bg-white/5 border-2 border-secondaryDark/10
+                        shadow-secondaryDark bg-secondaryDark/10 w-3/4"
+                        />
+                  </div>
+
+                  <Button title="Enviar" />
+                  <p className="font-primary dark:text-white/90 text-secondaryDark text-center mt-2">{status}</p>
+                </form>
+              </WrapperBlur>
             </section>
+          </section>
         </section>
-        </motion.div>
+      </motion.div>
     </>
-  )
+  );
 }
--- src/pages/Contact/elements/Input.jsx
+++ src/pages/Contact/elements/Input.jsx
@@ -1,23 +1,11 @@
-export default function Input({ type, title, placeholder }) {
-
-  const isTextarea = type === "textarea";
-
-  const inputClasses = `
-    rounded-full ml-2 backdrop-blur-md
-    p-2 shadow-lg drop-shadow-custom mb-4 text-secondaryDark dark:text-white
-    dark:shadow-white/10 dark:bg-white/5 border-2 border-secondaryDark/10
-    shadow-secondaryDark bg-secondaryDark/10`;
-
-  const textareaClasses = `
-    rounded-xl ml-2 backdrop-blur-md 
-    p-2 h-40 shadow-lg drop-shadow-custom mb-2 text-secondaryDark dark:text-white block mx-auto
-    dark:shadow-white/10 dark:bg-white/5 border-2 border-secondaryDark/10
-    shadow-secondaryDark bg-secondaryDark/10
-    w-3/4`;
-
-  return isTextarea ? (
-    <textarea name={title} id={title} placeholder={placeholder} className={textareaClasses}/>
-  ) : (
-    <input type={type} name={title} id={title} placeholder={placeholder} className={inputClasses}/>
-  );
+export default function Input({type,name,title,placeholder}) {
+  return (
+    <>
+      <input type={type} name={name} title={title} placeholder={placeholder} className='
+      rounded-full ml-2 backdrop-blur-md flex flex-row w-3/4
+      p-2 shadow-lg drop-shadow-custom mb-4 text-secondaryDark dark:text-white
+      dark:shadow-white/10 dark:bg-white/5 border-2 border-secondaryDark/10
+      shadow-secondaryDark bg-secondaryDark/10' />
+    </>
+  )
 }
